7312b598a5a3c5675772ec845c5903561fb9bf37
Daniel Winsvold <danielwinsvold@gmail.com>
2025-03-03 08:26:05 +0100

    norsk sanity studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,9 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/cms/[[...tool]]/page.tsx` route
  */
 
+import { StudioLayout } from "@/sanity/StudioLayout";
+import { linkToFrontpage } from "@/sanity/tools/linkToFrontpage";
+import { nbNOLocale } from "@sanity/locale-nb-no";
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
@@ -11,8 +14,6 @@ import { structureTool } from "sanity/structure";
 import { apiVersion, dataset, projectId } from "./src/sanity/env";
 import { schema } from "./src/sanity/schemaTypes";
 import { structure } from "./src/sanity/structure";
-import { StudioLayout } from "@/sanity/StudioLayout";
-import { linkToFrontpage } from "@/sanity/tools/linkToFrontpage";
 
 export default defineConfig({
   title: "Hamarøy IL",
@@ -24,6 +25,7 @@ export default defineConfig({
     structureTool({ structure }),
     visionTool({ defaultApiVersion: apiVersion }),
     unsplashImageAsset(),
+    nbNOLocale(),
   ],
   tools: [linkToFrontpage()],
   studio: {
