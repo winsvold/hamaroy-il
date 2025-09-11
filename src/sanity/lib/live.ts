fd8eb7f579f708ca3616ffca05649e17cd31e948
Daniel Winsvold <danielwinsvold@gmail.com>
2025-09-11 21:55:04 +0200

    typefiks

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,13 +1,13 @@
 // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity/live";
-import { sanityClient } from "./client";
+// import { defineLive } from "next-sanity/live";
+// import { sanityClient } from "./client";
 
-export const { sanityFetch: sanityLiveFetch, SanityLive } = defineLive({
-  client: sanityClient.withConfig({
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: "vX",
-  }),
-});
+// export const { sanityFetch: sanityLiveFetch, SanityLive } = defineLive({
+//   client: sanityClient.withConfig({
+//     // Live content is currently only available on the experimental API
+//     // https://www.sanity.io/docs/api-versioning
+//     apiVersion: "vX",
+//   }),
+// });
